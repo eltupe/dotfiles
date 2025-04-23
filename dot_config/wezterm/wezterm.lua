86381a80e73dee4e42db513e9bf5469d5a8edba4
Michael Schlegel <eltupe@protonmail.ch>
2025-04-23 11:52:24 +0200

    Update

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+config.color_scheme = 'Github'
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 13
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+config.keys = {
+ -- Clears the scrollback and viewport leaving the prompt line the new first line.
+  {
+    key = 'K',
+    mods = 'CMD|SHIFT',
+    action = act.ClearScrollback 'ScrollbackAndViewport',
+  },
+}
+
+return config
